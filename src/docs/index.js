446415f44df78f34d0d8a5756f286a6b691c6fa6
Justin Rhodes <justin.rhodes@studiocity.pro>
2018-10-18 12:55:13 +0100

    removed react-scrollchor from docs

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Scrollchor from 'react-scrollchor';
-// import '../assets/favicon.ico';
 
 import Header from './Header';
 import Intro from './Content/Intro';
@@ -43,17 +41,17 @@ class Demo extends Component {
           <div className='side'>
             <div className='sticky'>
               <ul>
-                <li><Scrollchor to="#install">Installation</Scrollchor></li>
-                <li><Scrollchor to="#basic">Basic Usage</Scrollchor></li>
-                <li><Scrollchor to="#advanced">Advanced Usage</Scrollchor></li>
-                <li><Scrollchor to="#examples">Examples</Scrollchor></li>
+                <li><a href='#install'>Installation</a></li>
+                <li><a href='#basic'>Basic Usage</a></li>
+                <li><a href='#advanced'>Advanced Usage</a></li>
+                <li><a href='#examples'>Examples</a></li>
                 <ul className='subUl'>
-                  <li className='subList'><Scrollchor to="#animations">Animations</Scrollchor></li>
-                  <li className='subList'><Scrollchor to="#behaviour">Behaviour</Scrollchor></li>
-                  <li className='subList'><Scrollchor to="#colors">Colors / Shapes</Scrollchor></li>
-                  <li className='subList'><Scrollchor to="#positions">Positions</Scrollchor></li>
+                  <li className='subList'><a href='#animations'>Animations</a></li>
+                  <li className='subList'><a href='#behaviour'>Behaviour</a></li>
+                  <li className='subList'><a href='#colors'>Colors / Shapes</a></li>
+                  <li className='subList'><a href='#positions'>Positions</a></li>
                 </ul>
-                <li><Scrollchor to="#api">API</Scrollchor></li>
+                <li><a href='#api'>API</a></li>
               </ul>
             </div>
           </div>
