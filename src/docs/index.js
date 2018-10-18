c749476bb0feb63afb2cb1f368ca375b6baff4b2
Justin Rhodes <justin.rhodes@studiocity.pro>
2018-10-18 18:24:34 +0100

    refactored logo & docs

--- src/docs/index.js
+++ src/docs/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import Header from './Header';
+import SideNav from './SideNav';
 import Intro from './Content/Intro';
 import Examples from './Content/Examples';
 import Behaviour from './Content/Examples/Behaviour';
@@ -39,21 +40,7 @@ class Demo extends Component {
         <Header />
         <div className='content'>
           <div className='side'>
-            <div className='sticky'>
-              <ul>
-                <li><a href='#install'>Installation</a></li>
-                <li><a href='#basic'>Basic Usage</a></li>
-                <li><a href='#advanced'>Advanced Usage</a></li>
-                <li><a href='#examples'>Examples</a></li>
-                <ul className='subUl'>
-                  <li className='subList'><a href='#animations'>Animations</a></li>
-                  <li className='subList'><a href='#behaviour'>Behaviour</a></li>
-                  <li className='subList'><a href='#colors'>Colors / Shapes</a></li>
-                  <li className='subList'><a href='#positions'>Positions</a></li>
-                </ul>
-                <li><a href='#api'>API</a></li>
-              </ul>
-            </div>
+            <SideNav />
           </div>
           <div className='main'>
             <section id={'install'}><Intro /></section>
